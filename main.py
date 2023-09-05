de70739111b91d638e34a0bbbd9a5ebe6d7185b8
adcrry <ducorroyalexandre@gmail.com>
2023-09-05 13:44:35 +0200

    add ask question

--- main.py
+++ main.py
@@ -18,3 +18,16 @@ def prompt():
     question = request.form['prompt']
     answer = ask_question_to_pdf.ask_question_to_pdf(question)
     return {"answer": answer}
+
+@app.route("/question", methods=['GET'])
+def question():
+    answer = ask_question_to_pdf.question()
+    return {"answer": answer}
+
+@app.route("/answer", methods=['POST'])
+def answer():
+    print(request.form)
+    user_answer = request.form['prompt']
+    question = request.form['question']
+    answer = ask_question_to_pdf.correct_answer(user_answer, question)
+    return {"answer": answer}
--- src/utils/ask_question_to_pdf.py
+++ src/utils/ask_question_to_pdf.py
@@ -92,4 +92,10 @@ def gpt3_completion(content):
 )['choices'][0]['message']['content']
 
 def ask_question_to_pdf(question):
-    return gpt3_completion(text + " D'après le texte précédent, réponds à la question suivantes: " + question)
+    return gpt3_completion(text + " D'après le texte précédent, réponds à la question suivantes: " + question + " Si ce n'est pas une question demande à l'élève de te poser une question. ")
+
+def question():
+    return gpt3_completion(text + " Pose moi une question sur le texte précédent.")
+
+def correct_answer(answer, question):
+    return gpt3_completion(text + " La question était: " + question + ". La réponse de l'élève est: " + answer + ". Corrige cette réponse si elle est fausse.")
